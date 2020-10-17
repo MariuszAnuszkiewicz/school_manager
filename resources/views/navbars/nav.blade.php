a3bff070c3947ce82124a1e7933aecb76aeaaf52
Mariusz Anuszkiewicz <mariuszanuszkiewicz82@gmail.com>
2020-10-17 14:33:22 +0200

    modify gitignore and route

--- resources/views/navbars/nav.blade.php
+++ resources/views/navbars/nav.blade.php
@@ -1,6 +1,6 @@
 <div class="container">
     <a class="navbar-brand" href="{{ url('/home') }}">
-        {{ 'Home' }}
+        {{ config('app.name') }}
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
         <span class="navbar-toggler-icon"></span>
