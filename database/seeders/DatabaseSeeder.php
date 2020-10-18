2888bb3c8e0b407efb138bd3e62c2b885d67cc5a
Mariusz Anuszkiewicz <mariuszanuszkiewicz82@gmail.com>
2020-10-18 16:23:31 +0200

    subjects table migration with linked things

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(SemesterTableSeeder::class);
         $this->call(ClassInSchoolTableSeeder::class);
+        $this->call(SubjectTableSeeder::class);
         $this->call(LessonPlanTableSeeder::class);
     }
 }
