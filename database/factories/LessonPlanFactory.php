60eb6a8a572a80c6279a9c09035b5e101aefe541
Mariusz Anuszkiewicz <mariuszanuszkiewicz82@gmail.com>
2020-10-18 16:38:03 +0200

    modify lesson plan factory

--- database/factories/LessonPlanFactory.php
+++ database/factories/LessonPlanFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\LessonPlan;
 use App\Models\ClassInSchool;
+use App\Models\Subject;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Arr;
 
@@ -23,22 +24,13 @@ class LessonPlanFactory extends Factory
      */
     public function definition()
     {
-        $subjects = [
-            'Chemistry',
-            'Biology',
-            'Geography',
-            'Mathematics',
-            'Phisics',
-            'English',
-        ];
-
         return [
             'class_in_school_id' => Arr::random([ClassInSchool::all()[rand(0, count(ClassInSchool::all()) - 1)]->id]),
-            'monday' => $this->faker->randomElement($subjects),
-            'tuesday' => $this->faker->randomElement($subjects),
-            'wednesday' => $this->faker->randomElement($subjects),
-            'thursday' => $this->faker->randomElement($subjects),
-            'friday' => $this->faker->randomElement($subjects),
+            'monday' => Arr::random([Subject::all()[rand(0, count(Subject::all()) - 1)]->name]),
+            'tuesday' => Arr::random([Subject::all()[rand(0, count(Subject::all()) - 1)]->name]),
+            'wednesday' => Arr::random([Subject::all()[rand(0, count(Subject::all()) - 1)]->name]),
+            'thursday' => Arr::random([Subject::all()[rand(0, count(Subject::all()) - 1)]->name]),
+            'friday' => Arr::random([Subject::all()[rand(0, count(Subject::all()) - 1)]->name]),
         ];
     }
 }
