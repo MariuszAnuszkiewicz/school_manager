79120dc5d0582af9c6ddf34ee656ba393ae7cdd1
Mariusz Anuszkiewicz <mariuszanuszkiewicz82@gmail.com>
2020-10-19 18:17:48 +0200

    presences table migration with linked things

---
+++ database/migrations/2020_10_19_152354_create_presences_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePresencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presences', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('pupil_id')->unsigned();
+            $table->foreign('pupil_id')->references('id')->on('pupils')->onDelete('cascade');
+            $table->string('presence', 3)->nullable();
+            $table->date('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presences');
+    }
+}
