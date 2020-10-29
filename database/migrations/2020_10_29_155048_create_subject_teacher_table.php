2d61b824802626e24b93d5fdb20db53ad54d325c
Mariusz Anuszkiewicz <mariuszanuszkiewicz82@gmail.com>
2020-10-29 17:05:40 +0100

    add subject teacher pivot table with linked things

---
+++ database/migrations/2020_10_29_155048_create_subject_teacher_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubjectTeacherTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subject_teacher', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('subject_id')->unsigned();
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+            $table->bigInteger('teacher_id')->unsigned();
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subject_teacher');
+    }
+}
